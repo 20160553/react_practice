7db0393c64eeadb3a911b56a381c26a7493cf528
20160553 <97psh@naver.com>
2025-09-23 22:18:48 +0900

    5일차: State, 제어문, 반복문을 이용한 UI

--- 01_starting_project/src/App.jsx
+++ 01_starting_project/src/App.jsx
@@ -3,12 +3,26 @@ import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 import { useState } from 'react';
-
+import { CORE_CONCEPTS, EXAMPLES } from './data.js'
 function App() {
-  const [ selectedTopic, setSelectedTopic ] = useState('Please click a button');
+  const [selectedTopic, setSelectedTopic] = useState('components');
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
+  }
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    console.log(EXAMPLES[selectedTopic]);
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].title}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].title}</code>
+        </pre>
+      </div>);
   }
 
   return (
@@ -17,24 +31,35 @@ function App() {
       <main>
         <section id="core-concepts">
           <h2>Core Concept</h2>
+          {[<p>Hello</p>, <p>World</p>]}
           <ul>
-            <CoreConcept title="Components" description="Component Description" img={componentsImg} />
-            <CoreConcept title="Components1" description="Component 11Description" img={componentsImg} />
-            <CoreConcept title="Components2" description="Component 22Description" img={componentsImg} />
-            <CoreConcept title="Components3" description="Component 33Description" img={componentsImg} />
+            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept {...conceptItem} /> )}
+            <CoreConcept title="components" description="Component Description" img={componentsImg} />
+            <CoreConcept title="jsx" description="Component 11Description" img={componentsImg} />
+            <CoreConcept title="props" description="Component 22Description" img={componentsImg} />
+            <CoreConcept title="state" description="Component 33Description" img={componentsImg} />
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect('Components')}>Components</TabButton>
-            <TabButton onSelect={() => handleSelect('Jsx')}>Jsx</TabButton>
-            <TabButton onSelect={() => handleSelect('Props')}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect('React')}>React</TabButton>
+            <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>Jsx</TabButton>
+            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>React</TabButton>
           </menu>
-          {selectedTopic}
+          {!selectedTopic && <p>Please select a topic.</p>}
+          {!selectedTopic ? <p>Please select a topic.</p> : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].title}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].title}</code>
+              </pre>
+            </div>)
+          }
+          {tabContent}
         </section>
-        <h2>Time to get started!</h2>
       </main>
     </div>
   );
